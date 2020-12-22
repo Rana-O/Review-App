d4718d750a24c43bbcc2f8954f05e669a42209f7
Rana-O <r.000918000@gmail.com>
2020-12-22 11:37:00 +0900

    reviews/newの見た目をほどほどに整える

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,4 +2,11 @@ class ReviewsController < ApplicationController
   def index
     @reviews = Review.all
   end
+
+  def show
+    @review = Review.find_by(id: params[:id])
+  end
+
+  def new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get '/' => 'home#top'
   get 'about' => 'home#about'
-  get 'reviews/index' => 'reviews#index'
-  get 'reviews/:id' => 'reviews#show'
+  resources :reviews
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
