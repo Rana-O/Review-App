e9fd229c3015bb351718045ac0e6e436f1e9e12c
Rana-O <r.000918000@gmail.com>
2020-12-28 11:13:53 +0900

    user詳細ページにそのuserの投稿を表示

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,10 +1,26 @@
+# == Schema Information
+#
+# Table name: reviews
+#
+#  id         :integer          not null, primary key
+#  others     :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :integer
+#
+# Indexes
+#
+#  index_reviews_on_user_id  (user_id)
+#
 class Review < ApplicationRecord
   validates :others, presence: true, length: {maximum: 140}
   validates :user_id, presence: true
 
-  def user
-    return User.find_by(id: self.user_id)
-  end
+  belongs_to :user
+
+  # def user
+  #   return User.find_by(id: self.user_id)
+  # end
 
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,18 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  email      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  password   :string
+#
 class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 100}
   validates :email, presence: true, uniqueness: true, length: { maximum: 255}
   validates :password, presence: true
+
+  has_many :reviews
 end
